80ec52ec069ea396946aa4e13dcdb70654281d1a
Christopher Chmielewski <christopherchmielewski93@gmail.com>
2022-10-09 13:35:52 -0400

    Partial progress on exercise 1.1

--- ex1-1.scm
+++ ex1-1.scm
@@ -1,7 +1,17 @@
 ; Exercise 1.1 Page 42
 
 ; A recursive process to compute the recurrance relation
-(define (fn n) 
+(define (fn-rec n) 
   (if (< n 3)
     n
     (+ (fn (- n 1)) (* 2 (fn (- n 2))) (* 3 (fn (- n 3))))))
+
+
+(define (fn n)
+  (fn-iter 2 1 0 n))
+
+(define (fn-iter a b c count)
+  (if (< count 3)
+    (+ a (* 2 b) (* 3 c))
+    (fn-iter (- a 1) (- b 2) (- c 3) (- count 1))
+  ))
