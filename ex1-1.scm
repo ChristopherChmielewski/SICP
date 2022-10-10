6254d62020cbcf458b6ce0989056e58f65edaa29
Christopher Chmielewski <christopherchmielewski93@gmail.com>
2022-10-10 11:58:08 -0400

    Shortened comments in Ex. 1.1 since racket interpreter throws error if any given line is too long.

--- ex1-1.scm
+++ ex1-1.scm
@@ -11,18 +11,16 @@
   (fn-iter 2 1 0 n))
 
 (define (fn-iter a b c count)
-  (cond ((and (< count 3) (= a 2)) count) ; termination condition if inital value of n is < 3
-        ((< count 3) a) ; termination condition if count > 2
-        (else (fn-iter (+ a (* 2 b) (* 3 c)) a b (- count 1))) ; else iterate until count reaches termination condition
+  (cond ((and (< count 3) (= a 2)) count) ; term. condition if inital value of n is < 3
+        ((< count 3) a) ; term. condition if count > 2
+        (else (fn-iter (+ a (* 2 b) (* 3 c)) a b (- count 1))) ; else iterate until count reaches term. con.
   ))
 
 ; block scoped iterative version
 (define (fn n)
   (define (fn-iter a b c count)
-  (cond ((and (< count 3) (= a 2)) count) ; termination condition if inital value of n is < 3
-        ((< count 3) a) ; termination condition if count > 2
-        (else (fn-iter (+ a (* 2 b) (* 3 c)) a b (- count 1))) ; else iterate until count reaches termination condition
+  (cond ((and (< count 3) (= a 2)) count) ; term. condition if inital value of n is < 3
+        ((< count 3) a) ; term. condition if count > 2
+        (else (fn-iter (+ a (* 2 b) (* 3 c)) a b (- count 1))) ; else iterate until count reaches term. con.
   ))
   (fn-iter 2 1 0 n))
-
-
