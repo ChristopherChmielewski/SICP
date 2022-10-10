fb68b9a75ab0f2a838d637d1382892cff1bfd700
Christopher Chmielewski <christopherchmielewski93@gmail.com>
2022-10-10 11:49:46 -0400

    Completed exercise 1.1, including a block scoped definition of an iterative function to calculate the recurrance relation.

--- ex1-1.scm
+++ ex1-1.scm
@@ -4,14 +4,25 @@
 (define (fn-rec n) 
   (if (< n 3)
     n
-    (+ (fn (- n 1)) (* 2 (fn (- n 2))) (* 3 (fn (- n 3))))))
+    (+ (fn-rec (- n 1)) (* 2 (fn (- n 2))) (* 3 (fn (- n 3))))))
 
-
+; A iterative process to compute the recurrance relation
 (define (fn n)
   (fn-iter 2 1 0 n))
 
 (define (fn-iter a b c count)
-  (if (< count 3)
-    (+ a (* 2 b) (* 3 c))
-    (fn-iter (- a 1) (- b 2) (- c 3) (- count 1))
+  (cond ((and (< count 3) (= a 2)) count) ; termination condition if inital value of n is < 3
+        ((< count 3) a) ; termination condition if count > 2
+        (else (fn-iter (+ a (* 2 b) (* 3 c)) a b (- count 1))) ; else iterate until count reaches termination condition
   ))
+
+; block scoped iterative version
+(define (fn n)
+  (define (fn-iter a b c count)
+  (cond ((and (< count 3) (= a 2)) count) ; termination condition if inital value of n is < 3
+        ((< count 3) a) ; termination condition if count > 2
+        (else (fn-iter (+ a (* 2 b) (* 3 c)) a b (- count 1))) ; else iterate until count reaches termination condition
+  ))
+  (fn-iter 2 1 0 n))
+
+
