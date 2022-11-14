33ad04dfd1e04ee2eff70c13888c73d6a7952179
Christopher Chmielewski <christopherchmielewski93@gmail.com>
2022-11-14 10:31:44 -0500

    Added logrithmic solution for ex. 1-16

--- ex1-16 logrithmic.rkt
+++ ex1-16 logrithmic.rkt
@@ -1,9 +1,24 @@
 #lang sicp
 
-; logrithmic solution from https://sicp-solutions.net/post/sicp-solution-exercise-1-16/
+(define (fe b n)
+  (define (even? n)
+    (= (remainder n 2) 0)
+    )
+  (define (fe-iter b n a)
+    (cond ((= n 0) a)
+          ((even? n) (fe-iter (* b b) (/ n 2) a))
+          (else (fe-iter b (- n 1) (* a b)))
+          ))
+  (fe-iter b n 1)
+  )
 
-(define (fast-expt-iter a b n)
-  (cond ((= n 0) a)
-        ((even? n) (fast-expt-iter a (* b b) (/ n 2)))
-        (else (fast-expt-iter (* a b) b (- n 1)))
-        ))
+; test
+(fe 2 0)
+(fe 2 1)
+(fe 2 2)
+(fe 2 3)
+(fe 2 4)
+(fe 2 5)
+(fe 2 6)
+(fe 2 7)
+(fe 2 8)
