1b8d041ed8f62e1a2e37c162a5467f6a1de2dd1a
Christopher Chmielewski <christopherchmielewski93@gmail.com>
2023-01-06 11:11:16 -0500

    Added comments with reductions rules and base case to ex2-27, added additional test to ex2-27.

--- Chapter_2/ex2-27.rkt
+++ Chapter_2/ex2-27.rkt
@@ -1,12 +1,17 @@
 #lang sicp
 
+; Reduction rules and base case
+; Reduction rule 1: if element is not a pair, add to reverse list (rlist) and go to next element
+; Reduction rule 2: if element is a pair, reverse the element, add the element to rlist, and go to next element
+; Base case: if list is an empty list then return the rlist
+
 (define (deep-reverse lt)
   (define (reverse sublist rlist)
-    (cond ((null? sublist) rlist)
-          ((not (pair? (car sublist))) (reverse (cdr sublist) (add-to-rlist (car sublist) rlist)))
+    (cond ((null? sublist) rlist) ; base case: list is an empty list
+          ((not (pair? (car sublist))) (reverse (cdr sublist) (add-to-rlist (car sublist) rlist)))  ; reduction step for non-pair elements (rule 1)
           ((pair? (car sublist))
            (reverse (cdr sublist) (add-to-rlist (deep-reverse (car sublist)) rlist))
-           )
+           ) ; reduction step for elements that are themselves pairs (i.e. nested lists) (rule 2)
           ))
   (reverse lt nil)
   )
@@ -21,6 +26,7 @@
 (define c (list (list 1 2) (list 3 4)))
 (define d (list 1 (list 2 3)))
 (define e (list 1 2 3 4 (list 5 6 (list 7 8 (list 9 10 (list 11 12 (list (list 13 14))))))))
+(define f (list (list 1 (list 2 3)) 4 (list 5 6 (list 7 8 (list 9 10 (list 11 12 (list (list 13 14))))))))
 
 a
 (deep-reverse a)
@@ -40,4 +46,8 @@ d
 
 e
 (deep-reverse e)
+(newline)
+
+f
+(deep-reverse f)
 (newline)
